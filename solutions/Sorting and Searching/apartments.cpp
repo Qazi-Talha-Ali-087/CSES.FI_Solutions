6cf3048b0cfe987965f20cd5de6d50dcbc7dfa72
Qazi-Talha-Ali-087 <b22cs087@iitj.ac.in>
2024-03-17 18:04:21 +0530

    new solution of apartment.cpp

---
+++ solutions/Sorting and Searching/apartments.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,m,k;
+    cin>>n;
+    cin>>m;
+    cin>>k;
+    vector <int> desired_size(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>desired_size[i];
+    }
+    
+      return 0;
+}
---
+++ solutions/Sorting and Searching/towers.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;<
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    int count = 0;
+    int tower[n]={0};
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<count+1;j++)
+        {
+            
+        }
+    }
+      return 0;
+}
